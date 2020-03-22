1dba7eccf28e7b421b05f317e68c8ecc215e0745
mahesapama03adhiswara <mahesapamaadhiswara@gmail.com>
2020-03-22 14:03:41 +0800

    Hello Git & Java

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
-public class hello{
- piblic static void main (string[]args){
-  system.out.println("hello world!!!!!");
+public class Hello{
+ public static void main (String [ ] args){
+ System.out.println("Hello Git ! !");
+ System.out.println("Hello Java ! !");
  }
 }
